2be9bd9ca80e2c409d490099e127eb4638c781fe
Jose Barcenas <josebarcenas7@gmail.com>
2016-03-28 22:19:02 -0400

    animate.css and media queries
    added multiple css rules, starting with media queries for mobile
    displays
    
    logo made in Visio, added to navbar then resized to fit

--- profile/js/myfunctions.js
+++ profile/js/myfunctions.js
@@ -7,3 +7,21 @@ function parallax(){
 $(window).scroll(function(e){
     parallax();
 });
+
+(function(){
+
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
+
+})();
