d38fbae4fb2122250d8b3e00e2a539aec0ee070a
Jose Barcenas <josebarcenas7@gmail.com>
2016-03-31 19:26:31 -0400

    charts js and social media icon
    added highchairs to start showing data, added social media rules to
    show in different media queries

--- profile/js/charts.js
+++ profile/js/charts.js
@@ -1,67 +1,48 @@
-		$(document).ready(function() {
-			var options = {
-				chart: {
-					renderTo: 'container',
-					type: 'column'
-				},
-				title: {
-					text: 'Vgs/Id plot'
-				},
-				xAxis: {
-					categories: []
-				},
-				yAxis: {
-					title: {
-						text: 'A'
-					}
-				},
-				series: []
-			};
-
-			/*
-			 Load the data from the CSV file. This is the contents of the file:
-
-				Apples,Pears,Oranges,Bananas,Plums
-				John,8,4,6,5
-				Jane,3,4,2,3
-				Joe,86,76,79,77
-				Janet,3,16,13,15
-
-			 */
-			$.get('/js/data.csv', function(data) {
-				// Split the lines
-				var lines = data.split('\n');
-				$.each(lines, function(lineNo, line) {
-					var items = line.split(',');
-
-					// header line containes categories
-					if (lineNo == 0) {
-						$.each(items, function(itemNo, item) {
-							if (itemNo > 0) options.xAxis.categories.push(item);
-						});
-					}
-
-					// the rest of the lines contain data with their name in the first position
-					else {
-						var series = {
-							data: []
-						};
-						$.each(items, function(itemNo, item) {
-							if (itemNo == 0) {
-								series.name = item;
-							} else {
-								series.data.push(parseFloat(item));
-							}
-						});
-
-						options.series.push(series);
-
-					}
-
-				});
-
-				var chart = new Highcharts.Chart(options);
-			});
-
-
-		});
+$(function () {
+    $('#graph').highcharts({
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
+    });
+});
